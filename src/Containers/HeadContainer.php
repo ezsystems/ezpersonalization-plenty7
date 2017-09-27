c62de8bfe7a9f0abea266eb9559f6f4b2a58cd01
Vojin Janevski <vojin.janevski@logeecom.com>
2017-09-27 11:28:55 +0200

    Refactored container code

--- src/Containers/HeadContainer.php
+++ src/Containers/HeadContainer.php
@@ -16,45 +16,66 @@ class HeadContainer
 {
     const YOOCHOOSE_CDN_SCRIPT = '//event.yoochoose.net/cdn';
     const AMAZON_CDN_SCRIPT = '//cdn.yoochoose.net';
-
-    public function __construct()
-    {
-    }
+    /**
+     * @var TemplateService
+     */
+    private $templateService;
+    /**
+     * @var SettingsService
+     */
+    private $settingsService;
+    /**
+     * @var SessionStorageService
+     */
+    private $sessionStorage;
+    /**
+     * @var OrderRepositoryContract
+     */
+    private $orderRepositoryContract;
+    /**
+     * @var CustomerService
+     */
+    private $customerService;
 
     /**
-     * @param Twig $twig
+     * HeadContainer constructor.
      * @param TemplateService $templateService
      * @param SettingsService $settingsService
      * @param SessionStorageService $sessionStorage
      * @param OrderRepositoryContract $orderRepositoryContract
      * @param CustomerService $customerService
-     * @return string
      */
-    public function call(
-        Twig $twig,
+    public function __construct(
         templateService $templateService,
         SettingsService $settingsService,
         SessionStorageService $sessionStorage,
         OrderRepositoryContract $orderRepositoryContract,
         CustomerService $customerService
-    ): string
+    )
     {
-        $mandator = $settingsService->getSettingsValue('customer_id');
-        $customerId = $customerService->getContactId();
-        $plugin = $settingsService->getSettingsValue('plugin_id');
-        $ycOverwriteEndpoint = $settingsService->getSettingsValue('script_id');
-        $ycEnableSearch = $settingsService->getSettingsValue('search_enable');
-        $itemType = $settingsService->getSettingsValue('item_type');
+        $this->templateService = $templateService;
+        $this->settingsService = $settingsService;
+        $this->sessionStorage = $sessionStorage;
+        $this->orderRepositoryContract = $orderRepositoryContract;
+        $this->customerService = $customerService;
+    }
 
-        /** @var Data $dataHelper */
-        $dataHelper = pluginApp(Data::class);
+    /**
+     * @param Twig $twig
+     * @param Data $dataHelper
+     * @param WebstoreConfigurationService $webStoreConfig
+     * @return string
+     */
+    public function call(Twig $twig, Data $dataHelper, WebstoreConfigurationService $webStoreConfig): string
+    {
+        $customerId = $this->customerService->getContactId();
+        $ycEnableSearch = $this->settingsService->getSettingsValue('search_enable');
+        $itemType = $this->settingsService->getSettingsValue('item_type');
 
-        /** @var WebstoreConfigurationService $webstoreConfig */
-        $webstoreConfig = pluginApp(WebstoreConfigurationService::class);
-        $storeConf = $webstoreConfig->getWebstoreConfig()->toArray();
+        $storeConf = $webStoreConfig->getWebstoreConfig()->toArray();
+        $currentTemplate = $this->templateService->getCurrentTemplate();
 
-        $currentTemplate = $templateService->getCurrentTemplate();
-
+        $orderDetails = [];
         switch ($currentTemplate) {
             case 'tpl.item':
                 $currentPage = 'product';
@@ -68,40 +89,21 @@ class HeadContainer
             case 'tpl.home':
                 $currentPage = 'home';
                 break;
+            case 'tpl.confirmation':
+                $currentPage = 'buyout';
+                $orderDetails = $this->getOrderDetails();
+                break;
             default:
                 $currentPage = '';
                 break;
         }
 
-        $order = '';
-        if ($currentTemplate === 'tpl.confirmation') {
-            $orderId = $sessionStorage->getSessionValue(SessionStorageKeys::LATEST_ORDER_ID);
-            $order = $orderRepositoryContract->findOrderById($orderId)->toArray();
-            foreach ($order['orderItems'] as $orderItems) {
-                $orderDetails[] = [
-                    'itemId' => $orderItems['id'],
-                    'quantity' => $orderItems['quantity'],
-                    'price' => $orderItems['id'],
-                    'currency' => $orderItems['id'],
-                ];
-            }
-        }
-
-        if ($ycOverwriteEndpoint) {
-            $scriptOverwrite = (!preg_match('/^(http|\/\/)/', $ycOverwriteEndpoint) ? '//' : '') . $ycOverwriteEndpoint;
-            $scriptUrl = preg_replace('(^https?:)', '', $scriptOverwrite);
-        } else {
-            $scriptUrl = $settingsService->getSettingsValue('performance') == 1 ?
-                self::AMAZON_CDN_SCRIPT : self::YOOCHOOSE_CDN_SCRIPT;
-        }
-
-        $scriptUrl = rtrim($scriptUrl, '/') . '/';
-        $scriptUrl = $scriptUrl . "v1/{$mandator}{$plugin}/tracking.";
+        $scriptUrl = $this->getScriptUrl();
 
         $template = [
             'shopUrl' => $storeConf['domainSsl'] . '/',
             'webStoreId' => $dataHelper->getStoreId(),
-            'orderData' => json_encode($order),
+            'orderData' => json_encode($orderDetails),
             'currentPage' => $currentPage,
             'ycCustomerId' => (int)$customerId,
             'ycEnableSearch' => $ycEnableSearch,
@@ -111,5 +113,47 @@ class HeadContainer
         ];
 
         return $twig->render('Yoochoose::content.head', $template);
+    }
+
+    /**
+     * Returns array of order items
+     *
+     * @return array
+     */
+    private function getOrderDetails(): array
+    {
+        $result = [];
+        $orderId = $this->sessionStorage->getSessionValue(SessionStorageKeys::LATEST_ORDER_ID);
+        $order = $this->orderRepositoryContract->findOrderById($orderId)->toArray();
+        foreach ($order['orderItems'] as $orderItems) {
+            $result[] = [
+                'itemId' => $orderItems['id'],
+                'quantity' => $orderItems['quantity'],
+                'price' => $orderItems['id'],
+                'currency' => $orderItems['id'],
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     */
+    private function getScriptUrl()
+    {
+        $mandator = $this->settingsService->getSettingsValue('customer_id');
+        $plugin = $this->settingsService->getSettingsValue('plugin_id');
+        $ycOverwriteEndpoint = $this->settingsService->getSettingsValue('script_id');
+
+        if ($ycOverwriteEndpoint) {
+            $scriptOverwrite = (!preg_match('/^(http|\/\/)/', $ycOverwriteEndpoint) ? '//' : '') . $ycOverwriteEndpoint;
+            $scriptUrl = preg_replace('(^https?:)', '', $scriptOverwrite);
+        } else {
+            $scriptUrl = $this->settingsService->getSettingsValue('performance') == 1 ?
+                self::AMAZON_CDN_SCRIPT : self::YOOCHOOSE_CDN_SCRIPT;
+        }
+
+        return rtrim($scriptUrl, '/') . "/v1/{$mandator}{$plugin}/tracking.";
     }
 }
